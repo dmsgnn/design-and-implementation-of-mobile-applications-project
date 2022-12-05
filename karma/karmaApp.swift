82eb70d62dda27215ded4e6ddf2b6a4790a44917
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2022-12-05 18:09:57 +0100

    Initial Commit

---
+++ karma/karmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  karmaApp.swift
+//  karma
+//
+//  Created by Giovanni Demasi on 05/12/22.
+//
+
+import SwiftUI
+
+@main
+struct karmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
