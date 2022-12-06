118740f682baa953e0f870c1eec452eefd64d869
tommasobucaioni <tbucaioni@virgilio.it>
2022-12-06 09:53:45 +0100

    Create Registration View

---
+++ karma/Views/RegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationView.swift
+//  karma
+//
+//  Created by Tommaso Bucaioni on 06/12/22.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text("This is the RegistrationView")
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
