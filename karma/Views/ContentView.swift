137dc82b9a54aa3d608443036b3802ffb0d1ae2d
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2022-12-10 21:31:24 +0100

    Fixed "project.pbxproj" file and ContentView Preview

--- karma/Views/ContentView.swift
+++ karma/Views/ContentView.swift
@@ -40,6 +40,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let myEnvObject = AuthViewModel()
+        ContentView().environmentObject(myEnvObject)
     }
 }
