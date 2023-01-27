8caa27b164bed17324083b9815c1b2e6e00d04ae
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2023-01-27 15:54:54 +0100

    UITest working on firebase server

--- karmaTests/karmaTests.swift
+++ karmaTests/karmaTests.swift
@@ -7,11 +7,24 @@
 
 import XCTest
 @testable import karma
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 final class karmaTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        print("Setting up Firebase emulator localhost:8080")
+        FirebaseApp.configure()
+        Auth.auth().useEmulator(withHost: "127.0.0.1", port: 9099)
+        //Firestore.firestore().useEmulator(withHost: "127.0.0.1", port: 8080)
+        let settings = Firestore.firestore().settings
+        settings.host = "127.0.0.1:8080"
+        settings.isPersistenceEnabled = false
+        settings.isSSLEnabled = false
+        Firestore.firestore().settings = settings
     }
 
     override func tearDownWithError() throws {
--- karmaUITests/karmaUITests.swift
+++ karmaUITests/karmaUITests.swift
@@ -6,6 +6,9 @@
 //
 
 import XCTest
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 final class karmaUITests: XCTestCase {
 
@@ -50,7 +53,7 @@ final class karmaUITests: XCTestCase {
             let emailTextField = app.textFields["Email"]
             XCTAssertTrue(emailTextField.exists)
             emailTextField.tap()
-            emailTextField.typeText("uitest2@gmail.com")
+            emailTextField.typeText("uitebstj52@gmail.com")
             
             let passwordSecureTextField = app.secureTextFields["Password"]
             XCTAssertTrue(passwordSecureTextField.exists)
@@ -61,17 +64,18 @@ final class karmaUITests: XCTestCase {
             let signUpButton = app.buttons["Sign Up"]
             XCTAssertTrue(signUpButton.exists)
             signUpButton.tap()
+            sleep(1)
             
 //            to fix from here on, the button select a photo is not recognised
-//            let selectPhotoButton = app.buttons["select a photo"]
-//            XCTAssertTrue(selectPhotoButton.exists)
-//            selectPhotoButton.tap()
-//
-//            app/*@START_MENU_TOKEN@*/.scrollViews.otherElements.images["Foto, 30 marzo 2018, 9:14 PM"]/*[[".otherElements[\"Photos\"].scrollViews.otherElements",".otherElements[\"Foto, 30 marzo 2018, 9:14 PM, Foto, 08 agosto 2012, 11:55 PM, Foto, 08 agosto 2012, 11:29 PM, Foto, 08 agosto 2012, 8:52 PM, Foto, 09 ottobre 2009, 11:09 PM, Foto, 13 marzo 2011, 1:17 AM\"].images[\"Foto, 30 marzo 2018, 9:14 PM\"]",".images[\"Foto, 30 marzo 2018, 9:14 PM\"]",".scrollViews.otherElements"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
-//
-//            let continueButton = app.buttons["Continue"]
-//            XCTAssertTrue(continueButton.exists)
-//            continueButton.tap()
+            let selectPhotoButton = app.buttons["plus.circle"]
+            XCTAssertTrue(selectPhotoButton.exists)
+            selectPhotoButton.tap()
+
+            app/*@START_MENU_TOKEN@*/.scrollViews.otherElements.images["Foto, 30 marzo 2018, 9:14 PM"]/*[[".otherElements[\"Photos\"].scrollViews.otherElements",".otherElements[\"Foto, 30 marzo 2018, 9:14 PM, Foto, 08 agosto 2012, 11:55 PM, Foto, 08 agosto 2012, 11:29 PM, Foto, 08 agosto 2012, 8:52 PM, Foto, 09 ottobre 2009, 11:09 PM, Foto, 13 marzo 2011, 1:17 AM\"].images[\"Foto, 30 marzo 2018, 9:14 PM\"]",".images[\"Foto, 30 marzo 2018, 9:14 PM\"]",".scrollViews.otherElements"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
+
+            let continueButton = app.buttons["Continue"]
+            XCTAssertTrue(continueButton.exists)
+            continueButton.tap()
             
         }
     
