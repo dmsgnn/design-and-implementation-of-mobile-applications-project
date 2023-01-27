ae95ad04d3dafa7a3a38c47fd885d73fc15043e3
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2023-01-27 10:58:22 +0100

    Revert "app delegate removed"
    This reverts commit 391b668c4d1a77cc6bcdb8560e97df41a1e0b48d.

--- karma/karmaApp.swift
+++ karma/karmaApp.swift
@@ -14,20 +14,21 @@ struct karmaApp: App {
     
     @StateObject var viewModel = AuthViewModel()
     
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-    init() {
-        FirebaseApp.configure()
-
-    }
+    
+//    init() {
+//        FirebaseApp.configure()
+//        
+//    }
     
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 ContentView()
-                    .environmentObject(viewModel)
                 
             }
-            
+            .environmentObject(viewModel)
         }
     
     }
