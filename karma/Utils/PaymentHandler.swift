5508d79dde1498bc5036a2960b9af277dca51946
tommasobucaioni <tbucaioni@virgilio.it>
2023-01-30 12:38:43 +0100

    ApplePay button and functions added to project

--- karma/Utils/PaymentHandler.swift
+++ karma/Utils/PaymentHandler.swift
@@ -2,45 +2,72 @@
 //  PaymentHandler.swift
 //  karma
 //
-//  Created by Tommaso Bucaioni on 03/01/23.
+//  Created by Tommaso Bucaioni on 30/01/23.
 //
 
-//import Foundation
-//import UIKit
-//import PassKit
-//import SwiftUI
-//
-//extension PaymentButton {
-//    struct Representable: UIViewRepresentable {
-//        var action: () -> Void
-//        
-//        func makeCoordinator() -> Coordinator {
-//            Coordinator(action: action)
-//        }
-//        
-//        func makeUIView(context: Context) -> some UIView {
-//            context.coordinator.button
-//        }
-//        
-//        func updateUIView(_ uiView: UIViewType, context: Context) {
-//            context.coordinator.action = action
-//        }
-//    }
-//    
-//    class Coordinator: NSObject {
-//        var action: () -> Void
-//        var button = PKPaymentButton(paymentButtonType: .donate, paymentButtonStyle: .automatic)
-//        
-//        init(action: @escaping () -> Void) {
-//            self.action = action
-//            super.init()
-//            
-//            button.addTarget(self, action: #selector(callback(_:)), for: .touchUpInside)
-//        }
-//        
-//        @objc
-//        func callback(_ sender: Any) {
-//            action()
-//        }
-//    }
-//}
+import Foundation
+import PassKit
+
+typealias PaymentCompletionHandler = (Bool) -> Void
+
+class PaymentHandler: NSObject {
+    var paymentController: PKPaymentAuthorizationController?
+    var paymentSummaryItems = [PKPaymentSummaryItem]()
+    var paymentStatus = PKPaymentAuthorizationStatus.failure
+    var completionHandler: PaymentCompletionHandler?
+
+    static let supportedNetworks: [PKPaymentNetwork] = [.visa, .masterCard]
+    
+    func startPayment(total: Float, completion: @escaping PaymentCompletionHandler) {
+        completionHandler = completion
+        let total = PKPaymentSummaryItem(label: "Total", amount: NSDecimalNumber(string: "\(total).00"), type: .final)
+        paymentSummaryItems.append(total)
+        
+        let paymentRequest = PKPaymentRequest()
+        paymentRequest.paymentSummaryItems = paymentSummaryItems
+        paymentRequest.merchantIdentifier = "merchant.dima.karma"
+        paymentRequest.merchantCapabilities = .capability3DS
+        paymentRequest.countryCode = "IT"
+        paymentRequest.currencyCode = "EUR"
+        paymentRequest.supportedNetworks = PaymentHandler.supportedNetworks
+//        paymentRequest.shippingType = .delivery
+//        paymentRequest.shippingMethods = .none
+//        paymentRequest.requiredShippingContactFields = [.name, .postalAddress]
+        
+        paymentController = PKPaymentAuthorizationController(paymentRequest: paymentRequest)
+        paymentController?.delegate = self
+        paymentController?.present(completion: { (presented: Bool) in
+            if presented {
+                debugPrint("Presented payment controller")
+            } else {
+                debugPrint("Failed to present payment controller")
+            }
+        })
+    }
+}
+
+extension PaymentHandler: PKPaymentAuthorizationControllerDelegate {
+    func paymentAuthorizationController(_ controller: PKPaymentAuthorizationController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        let errors = [Error]()
+        let status = PKPaymentAuthorizationStatus.success
+        
+        self.paymentStatus = status
+        completion(PKPaymentAuthorizationResult(status: status, errors: errors))
+    }
+    
+    func paymentAuthorizationControllerDidFinish(_ controller: PKPaymentAuthorizationController) {
+        controller.dismiss {
+            DispatchQueue.main.async {
+                if self.paymentStatus == .success {
+                    if let completionHandler = self.completionHandler {
+                        completionHandler(true)
+                    }
+                } else {
+                    if let completionHandler = self.completionHandler {
+                        completionHandler(false)
+                    }
+                }
+            }
+        }
+    }
+}
--- karma/ViewModels/PaymentViewModel.swift
+++ karma/ViewModels/PaymentViewModel.swift
@@ -14,16 +14,21 @@ class PaymentViewModel: ObservableObject {
     @Published var didMakePayment = false
     let service = PaymentService()
     
+    let paymentHandler = PaymentHandler()
+    @Published private(set) var paymentSuccess = false
+    
     func makePayment(forCollection collection: Collection, ofAmount euros: Float) {
-        let destination = collection.uid
-        
-        service.makePayment(destinationId: destination, collection: collection, total: euros) {
-            success in
-            if success {
-                self.didMakePayment = true
-                
-            } else {
-                
+        paymentHandler.startPayment(total: euros) { success in
+            self.paymentSuccess = success
+            let destination = collection.uid
+            self.service.makePayment(destinationId: destination, collection: collection, total: euros) {
+                success in
+                if success {
+                    self.didMakePayment = true
+                    
+                } else {
+                    
+                }
             }
             
         }
