a159f3f4f24ff9a655a27f7b1bbdc28138082dbf
Giovanni Demasi <giovanni.demasi@mail.polimi.it>
2023-01-30 11:38:26 +0100

    Created collection mock methods

--- karmaTests/Mocks/CollectionServiceMock.swift
+++ karmaTests/Mocks/CollectionServiceMock.swift
@@ -7,50 +7,122 @@
 
 import Foundation
 import Combine
+import FirebaseFirestore
 
 class CollectionServiceMock : CollectionServiceProtocol {
     
     @Published private(set) var addToFavouriteIsCalled = false
+    @Published private(set) var uploadCollectionIsCalled = false
+    @Published private(set) var fetchCollectionsIsCalled = false
+    @Published private(set) var fetchSingleCollectionsIsCalled = false
+    @Published private(set) var fetchCollectionsUIDIsCalled = false
+    @Published private(set) var fetchFavouritesCollectionsIsCalled = false
+    @Published private(set) var checkIfUserLikedCollectionIsCalled = false
+    @Published private(set) var removeFromFavouriteIsCalled = false
+    @Published private(set) var deleteCollectionIsCalled = false
+    @Published private(set) var updateCollectionIsCalled = false
+
+
+    @Published private(set) var collections = [Collection] ()
+    @Published private(set) var favourites = [Collection] ()
+
     
     func uploadCollection(title: String, caption: String, amount: Float, image: String, completion: @escaping(Bool) -> Void){
-        
+        uploadCollectionIsCalled = true
+        var coll = Collection(title: title, caption: caption, amount: amount, currentAmount: 0, favourites: 0, participants: 1, collectionImageUrl: image, timestamp: Timestamp(date: Date()), uid: "")
+        collections.append(coll)
+        completion(true)
     }
     
     func fetchCollections(completion: @escaping([Collection]) -> Void){
-        
+        fetchCollectionsIsCalled = true
+        completion(collections)
     }
     
     func fetchSingleCollection(forCid cid: String, completion: @escaping(Collection) -> Void){
-        
+        fetchSingleCollectionsIsCalled = true
+        completion(collections.first(where: {$0.id == cid})!)
     }
     
     func fetchCollections(forUid uid: String, completion: @escaping([Collection]) -> Void){
-        
+        fetchCollectionsUIDIsCalled = true
+        var c = [Collection] ()
+        for i in collections{
+            if i.id == uid{
+                c.append(i)
+            }
+        }
+        completion(c)
     }
     
     func fetchFavouritesCollections(completion: @escaping([Collection]) -> Void){
-        
+        fetchFavouritesCollectionsIsCalled = true
+        completion(favourites)
     }
     
     func addToFavourite(_ collection: Collection, completion: @escaping() -> Void){
         addToFavouriteIsCalled = true
+        favourites.append(collection)
         completion()
         
     }
     
     func checkIfUserlikedCollection(_ collection: Collection, completion: @escaping(Bool) -> Void){
-        
+        checkIfUserLikedCollectionIsCalled = true
+        if(collection.didLike == true){
+            completion(true)
+        }
+        else{
+            completion(false)
+        }
     }
     
     func removeFromFavourite(_ collection: Collection, completion: @escaping() -> Void){
-        
+        removeFromFavouriteIsCalled = true
+        var k = 0
+        for i in favourites{
+            if i.id == collection.id{
+                favourites.remove(at: k)
+                break
+            }
+            k+=1
+        }
+        completion()
     }
     
     func deleteCollection(_ collection: Collection, completion: @escaping(Bool) -> Void){
-        
+        deleteCollectionIsCalled  = true
+        var k = 0
+        var removed = false
+        for i in collections{
+            if i.id == collection.id{
+                collections.remove(at: k)
+                removed = true
+                break
+            }
+            k+=1
+        }
+        if removed {
+            completion(true)
+        }
+        else{
+            completion(false)
+        }
     }
     
     func updateCollectionData(_ collection: Collection, title: String, description: String, amount: Float, completion: @escaping(Bool) -> Void){
-        
+        uploadCollectionIsCalled = true
+        var k = 0
+        var updated = false
+        for i in collections{
+            if i.id == collection.id{
+                var c = Collection(id: i.id ,title: title, caption: description, amount: amount, currentAmount: i.currentAmount, favourites: i.favourites, participants: i.participants, collectionImageUrl: i.collectionImageUrl, timestamp: i.timestamp, uid: i.uid)
+                collections.remove(at: k)
+                collections.append(c)
+                updated = true
+                break
+            }
+            k+=1
+        }
     }
 }
--- karmaTests/karmaTests.swift
+++ karmaTests/karmaTests.swift
@@ -14,31 +14,21 @@ import FirebaseFirestoreSwift
 import FirebaseStorage
 import Combine
 
-final class PropertyExpectation<T: AnyObject, V: Equatable>: XCTNSPredicateExpectation {
-    init(object: T, keyPath: KeyPath<T, V>, expectedValue: V) {
-        let predicate = NSPredicate(block: { _, _ in
-            return object[keyPath: keyPath] == expectedValue
-        })
-        super.init(predicate: predicate, object: nil)
-    }
-}
-
 final class karmaTests: XCTestCase {
     
-    private var cancellable = Set<AnyCancellable>()
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        print("Setting up Firebase emulator localhost:8080")
-        FirebaseApp.configure()
-        Auth.auth().useEmulator(withHost: "127.0.0.1", port: 9099)
-        Storage.storage().useEmulator(withHost: "127.0.0.1", port: 9199)
-        //Firestore.firestore().useEmulator(withHost: "127.0.0.1", port: 8080)
-        let settings = Firestore.firestore().settings
-        settings.host = "127.0.0.1:8080"
-        settings.isPersistenceEnabled = false
-        settings.isSSLEnabled = false
-        Firestore.firestore().settings = settings
+//        print("Setting up Firebase emulator localhost:8080")
+//        FirebaseApp.configure()
+//        Auth.auth().useEmulator(withHost: "127.0.0.1", port: 9099)
+//        Storage.storage().useEmulator(withHost: "127.0.0.1", port: 9199)
+//        //Firestore.firestore().useEmulator(withHost: "127.0.0.1", port: 8080)
+//        let settings = Firestore.firestore().settings
+//        settings.host = "127.0.0.1:8080"
+//        settings.isPersistenceEnabled = false
+//        settings.isSSLEnabled = false
+//        Firestore.firestore().settings = settings
         
     }
     
